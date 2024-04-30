0ecd4007bfa7eb22b16661adcf0a748560e788f9
Loubna Ben Allal <44069155+loubnabnl@users.noreply.github.com>
2024-04-30 22:00:26 +0200

    fix overflow

--- fulltext/search.py
+++ fulltext/search.py
@@ -1,15 +1,13 @@
-import json
-import time
-import re
-import sys
 import argparse
-import random
+import json
+import sys
+import time
+import os
 
 import requests
-from datasets import load_dataset
-
-
-N_PAGES = 1000
+import pandas as pd
+from datasets import Dataset, concatenate_datasets, load_dataset
+from tqdm import tqdm
 
 
 def get_args():
@@ -17,10 +15,37 @@ def get_args():
     parser.add_argument("--input_dataset", type=str, default="HuggingFaceTB/bisac_expanded_final")
     parser.add_argument("--n_topics", type=int, default=2000)
     parser.add_argument("--n_pages", type=int, default=1000)
+    parser.add_argument("--n_chunks", type=int, default=4)
+    parser.add_argument("--save_interval", type=int, default=1000)
+    parser.add_argument("--target_datadet_prefix", type=str, default="HuggingFaceTB/search")
     return parser.parse_args()
 
-def sample_pages(x):
-    return {"sample_100_pages": x["topic_hits"][:100]}
+
+def load_and_merge(directory_path, id, args):
+    """To avoid datasets pyarrow overflow:
+    1- load each json chunk in a pandas dataframe
+    2- explode the topic_hits column
+    3- convert each chunk to datasets and concatenate the subsets"""
+    json_files = [file for file in os.listdir(directory_path) if file.endswith('.json')]
+    print(f"Found {len(json_files)} chunks (={args.n_topics / args.save_interval - 1})")
+
+    print("Loading the chunks...")
+    data_subsets = []
+    for file in tqdm(json_files):
+        file_path = os.path.join(directory_path, file)
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+        df = pd.DataFrame(data)
+        df = df.explode("topic_hits")
+        data_subsets.append(Dataset.from_pandas(df))
+
+    print("Merging the chunks...")
+    merged_data = concatenate_datasets(data_subsets).remove_columns(["__index_level_0__"])
+    print(merged_data)
+    print(f"Sanity check on the pages from topic 0: {merged_data[0]['topic_hits']}")
+
+    merged_data.push_to_hub(f"{args.target_datadet_prefix}_{id}", private=True)
+    print(f"Done! The data is available at '{args.target_datadet_prefix}_{id}' 🔥")
 
 
 # wait until the server is up
@@ -36,32 +61,30 @@ while True:
         pass
 
 
-def get_query(sample):
-    topics = re.findall(r'\d+\.\s*(.+?)(?=\n\s*\d+\.|$)', sample["completion"], re.DOTALL)
-    topics = [topic.strip() for topic in topics]
-    topics_only = [topic.split(":")[0].strip("* ") for topic in topics]
-    return {"topics_only": topics_only, "topics": topics}
+args = get_args()
+data = load_dataset(args.input_dataset, split="train", cache_dir="/scratch/cosmo/.cache")
+data = data.shuffle(0).select(range(args.n_topics))
+# path where the data chunks will be saved
+id = f"{args.n_topics}t_{args.n_pages}p"
+os.makedirs(f"intermediate_data/{id}", exist_ok=True)
 
-
-def add_hits(sample, n_pages):
+intermediate_data = []
+for index in range(len(data)):
+    sample = data[index]
     sample["topic_hits"] = []
-    query = " / ".join([sample["top_category"].strip(), sample["subcategory"].strip(), sample["subtopic"].strip()])
-    sample["query"] = query
-    print(f"Query: {query}")
+    query = " / ".join([sample["top_category"].strip(), sample["subcategory"].strip(), sample["subtopic"].strip(),])
     while True:
         try:
             response = requests.post(
                 "http://127.0.0.1:9308/search",
-                data=json.dumps({
-                    "index": "fineweb",
-                    "size": n_pages,
-                    "query": {
-                        "match": {
-                            "content": query
-                        }
+                data=json.dumps(
+                    {
+                        "index": "fineweb",
+                        "size": args.n_pages,
+                        "query": {"match": {"content": query}},
                     }
-                }),
-                timeout=1000
+                ),
+                timeout=1000,
             )
             if response.status_code != 200:
                 print(response.text, file=sys.stderr)
@@ -69,25 +92,23 @@ def add_hits(sample, n_pages):
                 continue
             else:
                 hits = response.json()["hits"]["hits"]
-                sample["topic_hits"].append(hits)
+                sample["topic_hits"] = hits
                 break
         except requests.exceptions.ConnectionError as e:
             print(e, file=sys.stderr)
             time.sleep(5)
             continue
+    intermediate_data.append(sample)
 
-    return sample
+    save_interval = min(args.n_topics, args.save_interval)
+    if index > 0 and (index + 1) % save_interval == 0:
+        # Save data every save_interval topics and reinitialize intermediate dicts
+        save_path = f"intermediate_data/{id}/data_{index - save_interval}_{index}.json"
+        with open(save_path, "w") as fp:
+            json.dump(intermediate_data, fp)
+        print(f"💾 Saved intermediate data at {save_path}")
+        intermediate_data = []
 
-args = get_args()
-data = load_dataset(args.input_dataset, split="train", cache_dir="/scratch/cosmo/.cache")
-data = data.shuffle(0).select(range(args.n_topics))
-data = data.map(add_hits, fn_kwargs={"n_pages": args.n_pages})
-subset = data.map(lambda x: {"sample_100_pages": x["topic_hits"][:100]})
-
-name = f"search_bisac_{args.n_topics}_topics"
-name_subset = name + "_subset_20"
-subset.push_to_hub(f"HuggingFaceTB/{name_subset}", private=True)
-print("Subset saved 🧸")
-data.push_to_hub(f"HuggingFaceTB/{name}", private=True)
-print("Full data saved 🧸")
-
+# merge dataset and push to hub
+directory_path = f"intermediate_data/{id}"
+load_and_merge(directory_path, id, args)
